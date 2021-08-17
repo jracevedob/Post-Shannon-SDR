fe85a6eec736e5fb646b8fd560289ab72d556f79
jracevedob <jracevedob@gmail.com>
2021-08-17 10:16:07 +0200

    HW description

---
+++ HW_Spec/README.md
@@ -0,0 +1,23 @@
+<p align="center">
+<img alt="USRP N310" src="https://github.com/jracevedob/Post-Shannon-SDR/blob/main/Installation/N310isoExplode.png" width="800">
+</p>
+
+The operation of the device depends in great extend on the adequate configuration of the USRP through the UHD utilities. In the case of the N310, the following are the specifications that must be taken under consideration for the deployment of applications in GNURadio
+
+### Supported Sample Rates
+The USRP N300/N310 supports the three fixed Master Clock Rates listed below.
+
+**122.88 MHz**
+**125.00 MHz**
+**153.60 MHz**
+
+### Tunning range
+The transmission frequency, also known as tunning, can be adjusted from 10MHz to 6GHz.
+Additionally, the user can configure the following parameters:
+**4 RX DDC chains in FPGA**
+**4 TX DUC chain in FPGA**
+
+For more information, please refer to the vendor's documentation in the following [Link](https://files.ettus.com/manual/page_usrp_n3xx.html) and in the following [Link](https://kb.ettus.com/N300/N310).
+
+
+
--- Transmissions/SpectrumAnalyzer/spectrum_analyzer.py
+++ Transmissions/SpectrumAnalyzer/spectrum_analyzer.py
@@ -28,6 +28,7 @@ from gnuradio.filter import firdes
 import sip
 from gnuradio import analog
 from gnuradio import blocks
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio.fft import window
 import sys
@@ -80,8 +81,10 @@ class spectrum_analyzer(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 3.8e6
+        self.transition_bw = transition_bw = 1
+        self.samp_rate = samp_rate = 3.5e6
         self.gain = gain = 50
+        self.decimation = decimation = 10
         self.center_freq_trans = center_freq_trans = 2e9
 
         ##################################################
@@ -216,10 +219,10 @@ class spectrum_analyzer(gr.top_block, Qt.QWidget):
         self._qtgui_freq_sink_x_1_win = sip.wrapinstance(self.qtgui_freq_sink_x_1.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_1_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
-            1024, #size
+            2048, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
             center_freq_trans, #fc
-            samp_rate, #bw
+            samp_rate/decimation, #bw
             "", #name
             1,
             None # parent
@@ -257,6 +260,7 @@ class spectrum_analyzer(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(decimation, firdes.low_pass(1,samp_rate,samp_rate/(2*decimation), transition_bw), center_freq_trans, samp_rate)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
         self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, center_freq_trans, gain, 0, 0)
 
@@ -269,7 +273,8 @@ class spectrum_analyzer(gr.top_block, Qt.QWidget):
         self.connect((self.analog_sig_source_x_1, 0), (self.qtgui_freq_sink_x_1, 0))
         self.connect((self.analog_sig_source_x_1, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.uhd_usrp_sink_0, 0))
-        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))
 
 
     def closeEvent(self, event):
@@ -280,6 +285,13 @@ class spectrum_analyzer(gr.top_block, Qt.QWidget):
 
         event.accept()
 
+    def get_transition_bw(self):
+        return self.transition_bw
+
+    def set_transition_bw(self, transition_bw):
+        self.transition_bw = transition_bw
+        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.low_pass(1,self.samp_rate,self.samp_rate/(2*self.decimation), self.transition_bw))
+
     def get_samp_rate(self):
         return self.samp_rate
 
@@ -287,7 +299,8 @@ class spectrum_analyzer(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.center_freq_trans, self.samp_rate)
+        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.low_pass(1,self.samp_rate,self.samp_rate/(2*self.decimation), self.transition_bw))
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.center_freq_trans, self.samp_rate/self.decimation)
         self.qtgui_freq_sink_x_1.set_frequency_range(self.center_freq_trans, self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
         self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
@@ -301,13 +314,22 @@ class spectrum_analyzer(gr.top_block, Qt.QWidget):
         self.analog_sig_source_x_1.set_amplitude(self.gain)
         self.uhd_usrp_sink_0.set_gain(self.gain/2, 0)
 
+    def get_decimation(self):
+        return self.decimation
+
+    def set_decimation(self, decimation):
+        self.decimation = decimation
+        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.low_pass(1,self.samp_rate,self.samp_rate/(2*self.decimation), self.transition_bw))
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.center_freq_trans, self.samp_rate/self.decimation)
+
     def get_center_freq_trans(self):
         return self.center_freq_trans
 
     def set_center_freq_trans(self, center_freq_trans):
         self.center_freq_trans = center_freq_trans
         self.analog_sig_source_x_1.set_frequency(self.center_freq_trans)
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.center_freq_trans, self.samp_rate)
+        self.freq_xlating_fir_filter_xxx_0.set_center_freq(self.center_freq_trans)
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.center_freq_trans, self.samp_rate/self.decimation)
         self.qtgui_freq_sink_x_1.set_frequency_range(self.center_freq_trans, self.samp_rate)
         self.uhd_usrp_sink_0.set_center_freq(self.center_freq_trans, 0)
         self.uhd_usrp_source_0.set_center_freq(self.center_freq_trans, 0)
